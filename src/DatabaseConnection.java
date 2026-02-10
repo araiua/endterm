5d3d6e1065c00d37015aa960cf454097c0f161fe
cuchj0 <arai.uakit@mail.ru>
2026-02-10 11:46:44 +0500

    ''

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -3,6 +3,12 @@ public class DatabaseConnection {
         ActorData actorData = new ActorData();
         actorData.addActor("Tom", "Holland");
         actorData.addActor("Ktoto", "Tota");
+
+        MovieData movieData = new MovieData();
+        movieData.addMovie("Spider Man", "2024");
+        ovieData.addMovie("Kakoito Film", "1997");
         actorData.listActors();
+        movieData.listMovies();
     }
+
 }
--- src/data/MovieData.java
+++ src/data/MovieData.java
@@ -1,7 +1,72 @@
-package data;
+import java.sql.*;
 
 public class MovieData {
+
     private static final String url = "jdbc:postgresql://localhost:5432/endterm_oop";
-    private static final String user = "postgres";
-    private static final String password = "13062013";
+    private static final String user = "postgres";   // твой пользователь
+    private static final String password = "13062013"; // твой пароль
+
+    public void addMovie(String title, int year) {
+        String sql = "INSERT INTO movies (title, year) VALUES (?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, title);
+            ps.setInt(2, year);
+            ps.executeUpdate();
+
+            System.out.println("Movie added: " + title + " (" + year + ")");
+
+        } catch (SQLException e) {
+            System.out.println("Error while adding movie");
+            e.printStackTrace();
+        }
+    }
+
+    public void listMovies() {
+        String sql = "SELECT * FROM movies";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("mid") + " | " +
+                                rs.getString("title") + " (" +
+                                rs.getInt("year") + ")"
+                );
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error while listing movies");
+            e.printStackTrace();
+        }
+    }
+
+    public void findMovieById(int mid) {
+        String sql = "SELECT * FROM movies WHERE mid = ?";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, mid);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                System.out.println(
+                        rs.getInt("mid") + " | " +
+                                rs.getString("title") + " (" +
+                                rs.getInt("year") + ")"
+                );
+            } else {
+                System.out.println("Movie not found with id: " + mid);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error while finding movie");
+            e.printStackTrace();
+        }
+    }
 }
