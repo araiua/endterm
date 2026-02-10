1e6f9ea9cda6ddd8f7e656b361071a1b7157f04e
cuchj0 <arai.uakit@mail.ru>
2026-02-10 11:30:28 +0500

    z

--- src/data/MovieData.java
+++ src/data/MovieData.java
@@ -1,4 +1,7 @@
 package data;
 
 public class MovieData {
+    private static final String url = "jdbc:postgresql://localhost:5432/endterm_oop";
+    private static final String user = "postgres";
+    private static final String password = "13062013";
 }
